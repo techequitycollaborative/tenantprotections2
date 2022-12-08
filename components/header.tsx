e6dbddaa8a37b5533b1efac89058766134d2626b
Matt Brooks <matt@techequitycollaborative.org>
2022-12-07 22:56:13 -0800

    fixed meta img urls

--- components/header.tsx
+++ components/header.tsx
@@ -12,14 +12,20 @@ const Header = () => {
         name="twitter:image:alt"
         content="Tenant pressing buttons on calculator"
       />
-      <meta name="twitter:image" content="/img/calculator.svg" />
+      <meta
+        name="twitter:image"
+        content="https://tenantprotections.org/img/calculator.svg"
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Tenant Protections" />
       <meta
         property="og:description"
         content="A tool for California tenants to understand their rights under state and local rent control"
       />
-      <meta property="og:image" content="/img/calculator.svg" />
+      <meta
+        property="og:image"
+        content="https://tenantprotections.org/img/calculator.svg"
+      />
       <meta property="og:site_name" content="Tenant Protections" />
       <meta property="og:url" content="https://tenantprotections.org" />
       <meta property="og:type" content="website" />
