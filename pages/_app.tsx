8a4847d432a9832b02e703643e117bba909345da
Matt Brooks <matt@techequitycollaborative.org>
2022-12-07 21:43:19 -0800

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,28 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Script from 'next/script';
+
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7ER4BQQ6EP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7ER4BQQ6EP');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
